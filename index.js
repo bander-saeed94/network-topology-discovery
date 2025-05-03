b2d523f1b399cffcd4609c9c5599898d2a45fc2e
bander <bannadr1@hotmail.com>
2025-05-03 12:25:05 +0300

    stable layout

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Endpoint to trigger complete ARP discovery
 app.get('/api/discover', async (req, res) => {
     try {
-        const routerIP = '172.16.23.130'; // Replace with your router IP
+        const routerIP = '172.16.137.20';
+        // const routerIP = '172.16.23.130'; // Replace with your router IP
         const communityString = 'public'; // Replace if different
         const devices = await discoverCompleteARP(routerIP, communityString);
 
@@ -21,7 +22,8 @@ app.get('/api/discover', async (req, res) => {
 });
 app.get('/api/router/interfaces', async (req, res) => {
     try {
-        const routerIP = '172.16.23.130';
+        const routerIP = '172.16.137.20';
+        // const routerIP = '172.16.23.130';
         const interfaces = await discoverRouterInterfaces(routerIP);
         res.json(interfaces);
     } catch (error) {
@@ -29,25 +31,6 @@ app.get('/api/router/interfaces', async (req, res) => {
         res.status(500).send('Failed to retrieve router interfaces');
     }
 });
-// API endpoint for SNMP discovery
-// app.get('/api/discover', async (req, res) => {
-    // try {
-    //     const devices = await discoverNetwork('172.16.23.130'); // Router IP
-    //     // const devices = await discoverNetwork('172.16.23.128'); // Router IP
-
-    //     res.json(devices);
-    // } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send('Discovery failed');
-    // }
-    // try {
-    //     const devices = await discoverDevices(['192.168.1.1', '192.168.1.2', '172.16.23.130', '172.16.23.128']); // Replace with your subnet/IPs
-    //     res.json(devices);
-    // } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send('Discovery failed');
-    // }
-// });
 
 // Start server
 const PORT = 3000;
