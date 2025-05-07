4f0b10edfe7950491cea46c31eedbede5ee1c25a
bander <bannadr1@hotmail.com>
2025-05-08 00:55:13 +0300

    get default gateway ip

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { discoverDevices, discoverNetwork, discoverCompleteARP, discoverRouterInterfaces } = require('./snmp_discovery');
+const gateway = require('default-gateway');
+
+async function getGatewayIP() {
+    try {
+        const result = await gateway.v4();
+        console.log("Detected gateway IP:", result.gateway);
+        return result.gateway;
+    } catch (err) {
+        console.error("Error detecting gateway IP:", err);
+        throw err;
+    }
+}
 
 // Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -9,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Endpoint to trigger complete ARP discovery
 app.get('/api/discover', async (req, res) => {
     try {
-        const routerIP = '172.16.137.20';
+        const routerIP = await getGatewayIP();;
         // const routerIP = '172.16.23.130'; // Replace with your router IP
         const communityString = 'public'; // Replace if different
         const devices = await discoverCompleteARP(routerIP, communityString);
@@ -22,7 +34,7 @@ app.get('/api/discover', async (req, res) => {
 });
 app.get('/api/router/interfaces', async (req, res) => {
     try {
-        const routerIP = '172.16.137.20';
+        const routerIP = await getGatewayIP();;
         // const routerIP = '172.16.23.130';
         const interfaces = await discoverRouterInterfaces(routerIP);
         res.json(interfaces);
